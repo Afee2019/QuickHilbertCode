d8c7c3cfb6c7ff229a4239e79331fd8f72913be8
smrdc <lishaojun@supermap.com>
2013-10-04 10:46:55 +0800

    性能对比实验

--- vs2012/Hilbert/Hilbert.cpp
+++ vs2012/Hilbert/Hilbert.cpp
@@ -5,10 +5,107 @@
 #include "QuickHilbertCode.h"
 #include <fstream>
 
+//QuickHilbertCode by Li-Shaojun(李绍俊，lishaojun@supermap.com)
+double QHCHilbert(int nLevel)
+{
+	/********************************************/
+	//统计执行时间
+	/********************************************/
+	LARGE_INTEGER Frequency;//计数器频率   
+	LARGE_INTEGER start_PerformanceCount;//起始计数器   
+	LARGE_INTEGER end_PerformanceCount;//结束计数器   
+	double run_time; //运行时间   
+	QueryPerformanceFrequency(&Frequency);   
+	QueryPerformanceCounter(&start_PerformanceCount);   
+	/********************************************/
+
+	int nWidth = 1<<nLevel;
+	int nHeight = 1<<nLevel;
+	DWORD nKey = 0;
+
+	CQuickHilbertCode qTree;
+
+	for(WORD i=0;i<nHeight;i++)
+	{
+		for(WORD j=0;j<nWidth;j++)
+		{
+			nKey = qTree.GetQuickHilbertKey(j,i,nLevel);
+		}
+	}
+
+
+	/********************************************/
+	//统计执行时间
+	/********************************************/
+	QueryPerformanceCounter(&end_PerformanceCount);   
+	run_time = ( end_PerformanceCount.QuadPart - start_PerformanceCount.QuadPart ) / (double)Frequency.QuadPart;  
+	/********************************************/
+
+	return run_time;
+}
+
+
+//经典Faloutsos & Roseman算法
+double FRHilbert(int nLevel)
+{
+	/********************************************/
+	//统计执行时间
+	/********************************************/
+	LARGE_INTEGER Frequency;//计数器频率   
+	LARGE_INTEGER start_PerformanceCount;//起始计数器   
+	LARGE_INTEGER end_PerformanceCount;//结束计数器   
+	double run_time; //运行时间   
+	QueryPerformanceFrequency(&Frequency);   
+	QueryPerformanceCounter(&start_PerformanceCount);   
+	/********************************************/
+
+	int nWidth = 1<<nLevel;
+	int nHeight = 1<<nLevel;
+	DWORD nKey = 0;
+
+	CQuickHilbertCode qTree;
+
+	for(WORD i=0;i<nHeight;i++)
+	{
+		for(WORD j=0;j<nWidth;j++)
+		{
+			nKey = qTree.GetFRHilbertKey(j,i,nLevel);
+		}
+	}
+
+
+	/********************************************/
+	//统计执行时间
+	/********************************************/
+	QueryPerformanceCounter(&end_PerformanceCount);   
+	run_time = ( end_PerformanceCount.QuadPart - start_PerformanceCount.QuadPart ) / (double)Frequency.QuadPart;  
+	/********************************************/
+
+	return run_time;
+}
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	//重复执行n次，取平均值
+	int nTimes = 20;
+
+	double dQHCTime = 0.0;
+	double dFRTime = 0.0;
+	for(int i=1;i<15;i++)
+	{
+		//计算10次取平均值
+		for(int j=0;j<nTimes;j++)
+		{
+			dQHCTime += QHCHilbert(i);
+			dFRTime += FRHilbert(i);
+		}
+		printf("Lev=%ld\tQHCTime=%9f\tFRTime=%9f\tAR=%3f\n", i, dQHCTime/nTimes, dFRTime/nTimes, dFRTime/dQHCTime);
+	}
+	system ( "pause" );
+	return 0;
+
+
 	/********************************************/
 	//用于统计执行时间的代码
 	/********************************************/
@@ -20,7 +117,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	QueryPerformanceCounter(&start_PerformanceCount);   
 	/********************************************/
 
-	WORD nLevel = 4 ;
+	WORD nLevel = 13;
 	int nWidth = 1<<nLevel;
 	int nHeight = 1<<nLevel;
 	DWORD nKey = 0, nKey2 = 0;
@@ -33,8 +130,8 @@ int _tmain(int argc, _TCHAR* argv[])
  	{
 		for(WORD j=0;j<nWidth;j++)
 		{
-			nKey = qTree.GetQuickHilbertKey(j,i,nLevel);
-			//nKey2 = qTree.GetFRHilbertKey(j,i, nLevel);
+			//nKey = qTree.GetQuickHilbertKey(j,i,nLevel);
+			nKey2 = qTree.GetFRHilbertKey(j,i, nLevel);
 
 			//dif += abs(int(nKey2)-int(nKey));
 
@@ -43,7 +140,7 @@ int _tmain(int argc, _TCHAR* argv[])
 // 		printf("\n");
 	}
 
-	printf("%lf", dif);
+//	printf("%lf", dif);
 	
 	/********************************************/
 	//用于统计执行时间的代码
@@ -57,7 +154,6 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	return 0;
 }
-
 
 
 
